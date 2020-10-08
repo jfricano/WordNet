6695928697f7f86a8539e5b597200c0869d840e4
Jason Fricano <jason.fricano@gmail.com>
2020-10-08 23:39:35 +0800

    add field, constructor, method

--- Outcast.java
+++ Outcast.java
@@ -1,12 +1,28 @@
 public class Outcast {
+  private final WordNet wordnet;
+
   // constructor takes a WordNet object
   public Outcast(WordNet wordnet) {
-
+    this.wordnet = wordnet;
   }
 
   // given an array of WordNet nouns, return an outcast
   public String outcast(String[] nouns) {
-    return "";
+    String outkast = "";
+    int outkastDist = -1;
+
+    for (int i = 0; i < nouns.length; i++) {
+      int nounDist = 0;
+      for (int j = 0; j < nouns.length; j++) {
+        if (j == i) continue;
+        nounDist += wordnet.distance(nouns[i], nouns[j]);
+      }
+      if (nounDist > outkastDist) {
+        outkastDist = nounDist;
+        outkast = nouns[i];
+      }
+    }
+    return outkast;
   }
 
   // see test client below
